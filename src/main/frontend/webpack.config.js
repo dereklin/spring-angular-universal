f37e0b3aa8775345a81a2bf1d17abd320d746e8c
Derek Lin <derek.lin@blackrock.com>
2017-10-06 14:02:49 -0700

    add frontend

---
+++ src/main/frontend/webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+  target: 'node',
+  entry: {
+    server: path.resolve(process.cwd(), 'server.js')
+  },
+  output: {
+    path: path.resolve(process.cwd(), 'dist-serverbundle'),
+    filename: '[name].bundle.js'
+  },
+  resolve: {
+    modules: [
+      'node_modules'
+    ],
+    extensions: [
+      '.js'
+    ]
+  }
+};
